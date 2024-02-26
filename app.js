6a2302ec4180050a29e58324cab3148adde2c0a2
gauravbalodi123 <balodigaurav0@gmail.com>
2024-02-27 01:22:23 +0530

    Quotes app

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose')
+const seedDB = require('./seed');
+const quotesRoutes = require('./apis/quotesRoutes')
+const cors = require('cors');
+
+mongoose.connect('mongodb://127.0.0.1:27017/quotes-db')
+    .then(() => console.log('connection open'))
+    .catch((err) => console.log(err));
+
+;
+
+// jo data react se aaega use req.body mein json format mein ye store karaega
+app.use(express.json());
+
+app.use(cors());
+
+// this will seed the database
+// seedDB();
+
+
+app.use(quotesRoutes);
+
+
+
+app.get('/hello' , (req,res)=>{
+    // javascript object ko json mein convert kar dehga.
+    res.status(200).json({msg:"hello from quotes app server at 8000"});
+})
+
+
+
+
+const port = process.env.port || 8000;
+
+app.listen(port, () => {
+    console.log(`server started at port ${port}`);
+})
+
+
